e3e7560f0df8400c8ad0c6d3a8f8f6f9ae0b1b88
schebet <91207140+schebet@users.noreply.github.com>
2025-11-25 14:33:52 +0100

    Rename mojNoviPost to post-10

--- src/data/posts/moj-novi-post.ts
+++ src/data/posts/moj-novi-post.ts
@@ -3,7 +3,7 @@ import slika777Srcset from "@/assets/photos/777.JPG?w=640;768;1024;1280;1920&for
 import slika777Url from "@/assets/photos/777.JPG?w=1024&format=webp&quality=85";
 import type { BlogPost } from "../blogPosts";
 
-export const mojNoviPost: BlogPost = {
+export const post-10: BlogPost = {
   id: 11, // obavezno jedinstven broj za svaki post
   title: "Naslov mog teksta",
   author: "Ime i prezime novinara",
